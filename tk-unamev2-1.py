f1b2529b25804a8b8db4b8564b0588b39a280d17
Danny Jones <104730967+djones0405@users.noreply.github.com>
2023-04-16 14:50:07 -0400

    Added Tkinter gui (#1)
    This program generates a suggested username based on the user's first name and last name. The program takes up to the first 7 letters of the last name and the first letters of the first name to create a username, keeping the name spelling. If the generated username is not unique, the program removes letters from the last name and adds letters from the first name until a unique username is created.
    
    The program uses the Tkinter module to create a simple graphical user interface. The user inputs their first name and last name into the corresponding entry fields and clicks the "Submit" button. The program then generates a suggested username and displays it to the user. If the user wishes to edit the suggested username, they can click "yes" when prompted and enter a new username.
    
    The program checks for duplicate usernames in a CSV file named "usernames.csv". If a duplicate username is found, the program modifies the username until it is unique. The program also ensures that the generated username only uses letters from the first and last names provided by the user. If any other characters are used, an error message is displayed.
    
    To run the program, simply execute the "tk-uname.py" file. The program requires Python 3 and the Tkinter module to be installed.

---
+++ tk-unamev2-1.py
@@ -0,0 +1,186 @@
+import csv
+import re
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
+import string
+import random
+
+def check_duplicates(username, csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if username == row[0]:
+                return True
+    return False
+
+
+import csv
+import string
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
+import random
+
+
+def check_duplicates(username, csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if username == row[0]:
+                return True
+    return False
+
+
+def generate_username(first_name, last_name, csv_file):
+    attempts = 0
+    while attempts < 10:
+        # Keep up to 7 letters of last name and first letters of first name to create username
+        username = last_name[:7].lower() + first_name[:min(len(first_name), 8 - len(last_name))].lower()
+
+        # If username is too short, pad with zeros
+        if len(username) < 8:
+            username += '0' * (8 - len(username))
+
+        # If username already exists, modify it
+        while check_duplicates(username, csv_file):
+            if len(last_name) > 1:
+                last_name = last_name[:-1]  # Remove last letter of last name
+                username = last_name[:7].lower() + first_name[:min(len(first_name), 8 - len(last_name))].lower()
+            else:
+                first_name = first_name[1:]  # Remove first letter of first name
+                username = last_name[:7].lower() + first_name[:min(len(first_name), 8 - len(last_name))].lower()
+
+            # If username is too short, pad with zeros
+            if len(username) < 8:
+                username += '0' * (8 - len(username))
+
+        if len(username) == 8:
+            return username
+
+        attempts += 1
+
+    raise ValueError("Could not generate a unique username after 10 attempts")
+
+
+def on_submit():
+    first_name = first_name_entry.get().strip()
+    last_name = last_name_entry.get().strip()
+
+    try:
+        suggested_username = generate_username(first_name, last_name, "usernames.csv")
+    except ValueError as e:
+        messagebox.showerror("Error", str(e))
+        return
+
+    message = f"Suggested username: {suggested_username}. Do you want to edit the username before saving?"
+    action = messagebox.askquestion("Preview Username", message)
+
+    if action == 'yes':
+        new_username = simpledialog.askstring(
+            "Edit Username", f"Edit username {suggested_username}:")
+        if not new_username:
+            return
+        if not (set(new_username) <= set(first_name + last_name)):
+            messagebox.showerror(
+                "Invalid Username", "Username can only contain letters from first and last name.")
+            return
+        if check_duplicates(new_username, "usernames.csv"):
+            messagebox.showerror("Duplicate Username",
+                                 f"Username {new_username} is already taken.")
+            return
+        suggested_username = new_username
+
+    with open("usernames.csv", 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([suggested_username, first_name, last_name])
+        messagebox.showinfo("Suggested Username",
+                            f"Suggested username: {suggested_username}")
+        window.destroy()
+
+
+window = tk.Tk()
+window.title("Suggest Username")
+window.geometry("400x200")
+window.resizable(False, False)
+
+first_name_label = tk.Label(window, text="First Name:")
+first_name_label.pack()
+first_name_entry = tk.Entry(window)
+first_name_entry.pack()
+
+last_name_label = tk.Label(window, text="Last Name:")
+last_name_label.pack()
+last_name_entry = tk.Entry(window)
+last_name_entry.pack()
+
+submit_button = tk.Button(window, text="Submit", command=on_submit)
+submit_button.pack()
+
+window.mainloop()
+
+
+
+
+
+
+def on_submit():
+    first_name = first_name_entry.get().strip()
+    last_name = last_name_entry.get().strip()
+
+    csv_file = "usernames.csv"
+
+    suggested_username = generate_username(first_name, last_name, csv_file)
+
+    while True:
+        message = f"Suggested username: {suggested_username}. Do you want to edit the username before saving?"
+        action = messagebox.askquestion("Preview Username", message)
+
+        if action == 'yes':
+            new_username = simpledialog.askstring(
+                "Edit Username", f"Edit username {suggested_username}:")
+            if not new_username:
+                break
+            if not re.match(r'^[a-zA-Z0-9]+$', new_username):
+                messagebox.showerror(
+                    "Invalid Username", f"{new_username} is not a valid username.")
+                continue
+            if check_duplicates(new_username, csv_file):
+                messagebox.showerror("Duplicate Username",
+                                     f"Username {new_username} is already taken.")
+                continue
+            suggested_username = new_username
+        elif action == 'no':
+            break
+        else:
+            messagebox.showerror(
+                "Invalid Action", "Invalid action. Please try again.")
+            continue
+
+    with open(csv_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([suggested_username, first_name, last_name])
+        messagebox.showinfo("Suggested Username",
+                            f"Suggested username: {suggested_username}")
+        window.destroy()
+
+
+window = tk.Tk()
+window.title("Suggest Username")
+window.geometry("400x200")
+window.resizable(False, False)
+
+first_name_label = tk.Label(window, text="First Name:")
+first_name_label.pack()
+first_name_entry = tk.Entry(window)
+first_name_entry.pack()
+
+last_name_label = tk.Label(window, text="Last Name:")
+last_name_label.pack()
+last_name_entry = tk.Entry(window)
+last_name_entry.pack()
+
+submit_button = tk.Button(window, text="Submit", command=on_submit)
+submit_button.pack()
+
+window.mainloop()
